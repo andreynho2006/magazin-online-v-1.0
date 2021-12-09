382966272c948a099676697c66e83a3fa2f74b7c
Andrei Cirlan <andreynho2006@gmail.com>
2021-12-09 07:24:11 +0200

    itterate over products and display them on landing page

--- magazin-online-v-1.0/action-home.php
+++ magazin-online-v-1.0/action-home.php
@@ -1,1 +1,21 @@
-continut pentru actiunea home
+<?php 
+foreach ($arr_product as $product_id => $product) {
+?>
+
+<div>
+    <div>
+        <a href="index.php?action=product&product_id=<?php echo $product_id; ?>">
+        <?php echo $product['name'] ?></a>
+    </div>
+    <div>
+        <?php echo $product['description'] ?></a>
+    </div>
+    <div>
+        <?php echo $product['price'] ?> RON</a>
+    </div>
+</div>
+<hr>
+<?php 
+}
+
+?>
